44da2935a23b8110d5b69bc1562e5fd497ee34aa
SeregaTheDed <shtanko33333@mail.ru>
2021-12-10 23:02:48 +0300

    Добавьте файлы проекта.

---
+++ Nadejda2/Nadejda2/NumberTheory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nadejda2
+{
+    interface IGettable
+    {
+        int GetValue();
+        string Solution { get; set; }
+    }
+    public static class NumberTheory
+    {
+        private class NOD : IGettable
+        {
+            public int Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Solution { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        }
+
+    }
+}
