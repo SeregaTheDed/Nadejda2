08bd83db6c0b31353079f6b1b3fe58d00c88d463
DedEgor-creator <korepin.02@gmail.com>
2021-12-20 00:33:35 +0300

    NodPage Ready

--- Nadejda2/Nadejda2/StartPage.xaml.cs
+++ Nadejda2/Nadejda2/StartPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nadejda2.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,11 @@ namespace Nadejda2
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void Button_Clicked_Nod(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Nod());
         }
     }
 }
